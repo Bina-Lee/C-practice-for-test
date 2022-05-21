3196122afa67806a78b24b0bd40c83792a2e9e82
JS-Lee-0129 <awwdde22ljs@gmail.com>
2022-05-22 03:55:51 +0900

    암튼 내용 추가

--- fortest/2022.05.22 1차 기말고사 대비.c
+++ fortest/2022.05.22 1차 기말고사 대비.c
@@ -70,3 +70,62 @@ int main() {
 
 }
 #endif
+
+/*
+math.h
+
+floor 내림
+ceil 올림
+fabs 절댓값(function abs)
+pow a^b(a의 b승), 입력값 2개
+sqrt 제곱근(square root)
+
+변수정의
+
+static 지역변수에서 정의시 전역변수로 작용
+register 처리속도가 빨라지지만 컴파일러가 제외시킬가능성
+volatile 임베디드에서 주로 사용, ram의 값을 사용하게 함
+		register에 있는 값은
+		수시로 ram의 값이 바뀌는 임베디드에서는 부적합
+extern 다른 파일에 선언되어있는 변수
+		글로벌변수로 선언해줘야됨
+static 전역변수로 선언할 경우 extern으로 정의해서
+		다른 소스에서 사용할 수 없음
+
+문자입출력 라이브러리
+
+getchar buffering echo
+getch 둘다 없음
+getche echo
+	int ch = getch()
+	putchar(ch)
+	putch(ch)
+
+문자열 입출력 라이브러리
+gets(a)
+
+문자처리 라이브러리
+ctype.h
+
+isalpha
+isupper / islower
+isdigit / isalnum / isxdigit
+isspace  공백 탭
+ispunct 쉼표
+isprintf 가시성
+iscntrl 제어문자
+isascii
+toupper
+tolower
+
+문자열 처리함수
+string.h
+
+strlen
+strcmp 문자열 비교, 같으면 0을 출력 주의
+strcpy
+strcat 문자열 붙이기
+strchr 문자열에서 문자 주소 찾음
+strstr 문자열에서 문자열의 주소를 찾음
+
+*/
